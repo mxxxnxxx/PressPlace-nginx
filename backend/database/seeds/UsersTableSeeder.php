af2e7eef0b1edfed7785f90bc5c6996dbf1b968b
mxxxnxxx <mmm.mt.co.jp@gmail.com>
2022-03-16 17:11:33 +0900

    Userテーブルdemoアカウント追加

--- backend/database/seeds/UsersTableSeeder.php
+++ backend/database/seeds/UsersTableSeeder.php
@@ -11,6 +11,46 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
+        App\User::create([
+        'name' => '食べるの大介さん(demo)',
+        'age' => 55,
+        'email' => 'test1@test.mail.com',
+        'password' => Hash::make('testPlay'), // この場合、「my_secure_password」でログインできる
+        'introduction' => '食べるの大好きです!ずっと食べていたいです!(demo)',
+        'remember_token' => Str::random(10),
+        ]);
+        App\User::create([
+        'name' => '服好きと繋がりたい(代表)(demo)',
+        'age' => 22,
+        'email' => 'test2@test.mail.com',
+        'password' => Hash::make('testPlay'), // この場合、「my_secure_password」でログインできる
+        'introduction' => '服好きと繋がりたいと考えはじめてつぶやいてたらバズったのは私です｡(仮説) 主に行ったことがある服屋さんをpressします!!(demo)',
+        'remember_token' => Str::random(10),
+        ]);
+        App\User::create([
+        'name' => 'キャフェに住む人(demo)',
+        'age' => 55,
+        'email' => 'test3@test.mail.com',
+        'password' => Hash::make('testPlay'), // この場合、「my_secure_password」でログインできる
+        'introduction' => 'キャフェを営む50代の男性です｡自分の店は載せませんが｡｡尊敬しているお店をのせていきます｡(demo)',
+        'remember_token' => Str::random(10),
+        ]);
+        App\User::create([
+        'name' => 'サンドの飯よりart好き(demo)',
+        'age' => 44,
+        'email' => 'test4@test.mail.com',
+        'password' => Hash::make('testPlay'), // この場合、「my_secure_password」でログインできる
+        'introduction' => 'art好きです｡気難しくないので絡んでね｡',
+        'remember_token' => Str::random(10),
+        ]);
+        App\User::create([
+        'name' => 'サックスピストルズ:カスタネット担当(demo)',
+        'age' => 55,
+        'email' => 'test5@test.mail.com',
+        'password' => Hash::make('testPlay'), // この場合、「my_secure_password」でログインできる
+        'introduction' => 'カスタネットで世界を救う人です｡演奏したことがあるライブハウスを載せます(demo)',
+        'remember_token' => Str::random(10),
+        ]);
         // testUserを定義
         App\User::create([
         'name' => 'testUser',
