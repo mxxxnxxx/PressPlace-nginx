a6a5e0094341cff4005fc44c757424d722b78005
mxxxnxxx <mmm.mt.co.jp@gmail.com>
2022-03-23 06:43:57 +0900

    placeCardHeaderfix

--- backend/resources/ts/place/components/molecules/PlaceCardHeader.tsx
+++ backend/resources/ts/place/components/molecules/PlaceCardHeader.tsx
@@ -22,17 +22,19 @@ const useStyle = makeStyles(() => ({
         }
     },
     avatar: {
-        margin: '0'
+        margin: '0',
+        flexShrink: 1,
     },
     action: {
+        flexShrink: 1,
         alignSelf: 'center',
-        margin: '0'
+        margin: '0',
     },
     name: {
-        width: '50%',
-        whiteSpace: 'nowrap',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis'
+        display: "-webkit-box",
+        overflow: "hidden",
+        "-webkit-line-clamp": 1,
+        "-webkit-box-orient": "vertical",
     }
 }))
 
@@ -46,9 +48,8 @@ const PlaceCardHeader: React.FC<Props> = ({
         <CardHeader
             className={classes.root}
             classes={{
-
-                action: classes.action,
                 avatar: classes.avatar,
+                action: classes.action,
             }}
             avatar={
                 <>
