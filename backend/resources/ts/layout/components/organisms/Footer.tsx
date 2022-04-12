e9626ad5c326efd4e95f86a60685cd3e1e39e441
mxxxnxxx <mmm.mt.co.jp@gmail.com>
2022-04-12 12:29:54 +0900

    aboutMe Link 追加

--- backend/resources/ts/layout/components/organisms/Footer.tsx
+++ backend/resources/ts/layout/components/organisms/Footer.tsx
@@ -29,7 +29,7 @@ const Footer: FC = () => {
                             title="pressplaceとは？"
                             linkUrl="/about"
                         />
-                        <FooterNavItem type="internal" title="作った人" linkUrl="/creator" />
+                        <FooterNavItem type="external" title="作った人" linkUrl="http://35.74.155.2/" />
                     </List>
                 </Grid>
                 <Grid item xs={12} sm={4} component="nav">
