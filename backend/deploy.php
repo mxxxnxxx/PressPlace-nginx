58f5c14baedeb637e3e867afc98c196c0e0c6a01
mxxxnxxx <mmm.mt.co.jp@gmail.com>
2022-04-20 20:14:41 +0900

    fix

--- backend/deploy.php
+++ backend/deploy.php
@@ -3,43 +3,44 @@
 declare(strict_types=1);
 
 namespace Deployer;
-
+use Symfony\Component\Console\Input\InputOption;
 require 'recipe/laravel.php';
 
-// Project name
+// プロジェクト名（自分のプロジェクト名を設定）
 set('application', 'PressPlace');
 
-// Project repository
-set('repository', '');
-
-// [Optional] tty で名前をわりあてるか
+// [Optional] gitcloneにttyを割り当てる。デフォルト値はfalse。
 set('git_tty', false);
 
-// ファイル共有
+// デプロイ間の共有ファイル/ディレクトリ
 add('shared_files', []);
 add('shared_dirs', []);
 
-// Webサーバーによって書き込み可能か
+// Webサーバーによる書き込み可能なディレクトリ
 add('writable_dirs', []);
 set('allow_anonymous_stats', false);
 
-// デプロイ先のサーバー情報をほかファイルで記述 yml形式
 inventory('servers.yml');
 
-// gitクローン時に実行される こと
-task('build', function (): void {
-    ('cd {{ release_path }} && build');
+// タスク
+task('build', function () {
+('cd {{ release_path }} && build');
 });
 
-// [Optional] デプロイが失敗した場合、自動的にロックが解除される
+// [Optional] デプロイが失敗した場合、自動的にロックが解除される。
 after('deploy:failed', 'deploy:unlock');
 
 // シンボリックリンクの新しいリリースの前にデータベースを移行する。
 before('deploy:symlink', 'artisan:migrate');
-
-// release_pathに作業ディレクトリとして/backendを設定。
+// 本番環境用のenvファイルをコピーする
+before('deploy:shared','deploy:copy:env');
+//release_pathに作業ディレクトリとして/backendを設定。（自身の作業ディレクトリを指定してください。）
 after('deploy:update_code', 'set_release_path');
-task('set_release_path', function (): void {
-    $newReleasePath = get('release_path') . '/backend';
-    set('release_path', $newReleasePath);
+task('set_release_path', function () {
+$newReleasePath = get('release_path') . '/backend';
+set('release_path', $newReleasePath);
+});
+
+task('deploy:copy:env', function () {
+run("cp {{ release_path }}/.env.{{ stage }} {{ deploy_path }}/shared/.env");
 });
