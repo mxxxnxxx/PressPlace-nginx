851ccfa92aea7c31d3c25a09cb42b11322b38b8d
mxxxnxxx <mmm.mt.co.jp@gmail.com>
2022-04-20 20:24:47 +0900

    fix

--- backend/deploy.php
+++ backend/deploy.php
@@ -3,44 +3,69 @@
 declare(strict_types=1);
 
 namespace Deployer;
-use Symfony\Component\Console\Input\InputOption;
+
 require 'recipe/laravel.php';
 
-// プロジェクト名（自分のプロジェクト名を設定）
+// Project name
 set('application', 'PressPlace');
 
-// [Optional] gitcloneにttyを割り当てる。デフォルト値はfalse。
+// Project repository
+set('repository', '');
+
+// [Optional] tty で名前をわりあてるか
 set('git_tty', false);
 
-// デプロイ間の共有ファイル/ディレクトリ
+// ファイル共有
 add('shared_files', []);
 add('shared_dirs', []);
 
-// Webサーバーによる書き込み可能なディレクトリ
+
+// .gitignoreで.envはデプロイされない からのままだと deployerの処理で
+// .envがシンボルリンクになり処理dockerFileのコピーの処理でエラーになる
+
+// 回避するため実装
+
+// コマンドにオプションを作成
+// オプションのルールとして任意で値を入れる許可
+option('env-update', null, InputOption::VALUE_OPTIONAL, 'update env file.');
+task('copy:env', function (): void {
+    if (input()->hasOption('env-update')) {
+        $update = input()->getOption('env-update');
+
+        if ($update == 'true') {
+            $stage = get('stage');
+            $src = ".env.${stage}";
+            $path = get('deploy_path');
+            $shared_path = "${path}/shared";
+            run("if [ -e $(echo ${shared_path}/.env ) ]; then cp {{ release_path }}/${src} ${shared_path}/.env; fi");
+            run("cp {{ release_path }}/${src} {{ release_path }}/.env");
+        }
+    }
+});
+// タイミングをしていすることでかいひ
+
+before('deploy:shared','copy:env');
+// Webサーバーによって書き込み可能か
 add('writable_dirs', []);
 set('allow_anonymous_stats', false);
 
+// デプロイ先のサーバー情報をほかファイルで記述 yml形式
 inventory('servers.yml');
 
-// タスク
-task('build', function () {
-('cd {{ release_path }} && build');
+// gitクローン時に実行される こと
+task('build', function (): void {
+    ('cd {{ release_path }} && build');
 });
 
-// [Optional] デプロイが失敗した場合、自動的にロックが解除される。
+// [Optional] デプロイが失敗した場合、自動的にロックが解除される
 after('deploy:failed', 'deploy:unlock');
 
 // シンボリックリンクの新しいリリースの前にデータベースを移行する。
 before('deploy:symlink', 'artisan:migrate');
-// 本番環境用のenvファイルをコピーする
-before('deploy:shared','deploy:copy:env');
-//release_pathに作業ディレクトリとして/backendを設定。（自身の作業ディレクトリを指定してください。）
-after('deploy:update_code', 'set_release_path');
-task('set_release_path', function () {
-$newReleasePath = get('release_path') . '/backend';
-set('release_path', $newReleasePath);
-});
 
-task('deploy:copy:env', function () {
-run("cp {{ release_path }}/.env.{{ stage }} {{ deploy_path }}/shared/.env");
+// release_pathに作業ディレクトリとして/backendを設定。
+after('deploy:update_code', 'set_release_path');
+task('set_release_path', function (): void {
+    $newReleasePath = get('release_path') . '/backend';
+    set('release_path', $newReleasePath);
 });
